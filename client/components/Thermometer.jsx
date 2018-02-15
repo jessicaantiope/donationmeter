46488d52b43c2d76e64dc54af32c7dfe30d33e9a
kahu2018 <fake@fake.com>
2018-02-15 15:01:08 +1300

    SVG rectangle created

--- client/components/Thermometer.jsx
+++ client/components/Thermometer.jsx
@@ -1,2 +1,11 @@
+import React from 'react'
 
-// import React from 'react'
+const Thermometer = () => (
+  <div>
+      <svg width={300} height={400}>
+          <rect x="0" y="300" width="300" height="100" fill="red" fillOpacity="0.5" stroke="crimson" strokeWidth="4" />
+      </svg>
+  </div>
+)
+
+export default Thermometer
