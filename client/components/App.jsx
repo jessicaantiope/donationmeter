adcee5c1db74a0d0821a01d085513fe45f29ada1
Your Name <you@example.com>
2018-02-16 14:59:40 +1300

    Rebased from dev

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,6 +6,8 @@ import Total from './Total'
 import Thermometer from './Thermometer'
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
 import DonorTable from './DonorTable'
+import {makeDonation} from '../api'
+import { connect } from 'react-redux'
 
 class App extends React.Component {
   constructor(props) {
@@ -15,34 +17,42 @@ class App extends React.Component {
       showForm: false
     }
     this.displayForm = this.displayForm.bind(this)
+    this.handleDonation = this.handleDonation.bind(this)
   }
 
   displayForm(event) {
     this.setState({ showForm: true })
   }
 
+  handleDonation(donationDetails) {
+    let {amount, name} = donationDetails
+    this.props.dispatch(makeDonation(amount, name))
+  }
+
 
   render() {
     return (
       <Router>
-        <div className='app'>
-          <h1 className='title is-1'>hello</h1>
-          <button onClick={this.displayForm}>Button</button>
-          {this.state.showForm && <Form />}
-          <Thermometer />
-          <DonorTable />
-          <div>
-            <Route path="/DonorTable" component={DonorTable} />
-            <a href='/#/DonorTable'>DonorTable</a>
-
-          </div>
-        </div>
         <div>
-          <Leaderboard />
+          <div className='app'>
+            <h1 className='title is-1'>hello</h1>
+            <button onClick={this.displayForm}>Button</button>
+            {this.state.showForm && <Form />}
+            <Thermometer />
+            <DonorTable />
+            <div>
+              <Route path="/DonorTable" component={DonorTable} />
+              <a href='/#/DonorTable'>DonorTable</a>
+
+            </div>
+          </div>
+          <div>
+            <Leaderboard />
+          </div>
         </div>
       </Router>
     )
   }
 }
 
-export default App
+export default connect()(App)
